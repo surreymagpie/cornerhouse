d7d41415064b01316e171e09123982a14c7c1134
Rob Curtis <surreymagpie@yahoo.co.uk>
2024-06-21 21:22:09 +0100

    Change `run` command to shell function
    Shell alias was only receiving the first argument passed. Instead a
    shell function `run()` is now set so any/all arguments can be saved
    in a local variable and passed as one to the container.
    
    This will work as originally intended to execute any code in the
    container without entering it.

--- .aliases.sh
+++ .aliases.sh
@@ -1,4 +1,4 @@
 alias drush='podman exec -it ${CONTAINER_NAME} vendor/bin/drush'
 alias composer='podman exec -it ${CONTAINER_NAME} /usr/local/bin/composer'
 alias php='podman exec -it ${CONTAINER_NAME} /usr/bin/php'
-alias run='podman exec -it ${CONTAINER_NAME} /bin/bash -c'
+run () { local args="$@"; podman exec -it ${CONTAINER_NAME} /bin/bash -c $args; }

99eb074b1b1f0b3df6046e07267d93adab1697ba
Rob Curtis <surreymagpie@yahoo.co.uk>
2024-07-03 12:59:44 +0100

    Add GIT_EDITOR variable and podman-compose aliases

--- .aliases.sh
+++ .aliases.sh
@@ -1,3 +1,5 @@
+alias up="podman-compose up -d"
+alias down="podman-compose down"
 alias drush='podman exec -it ${CONTAINER_NAME} vendor/bin/drush'
 alias composer='podman exec -it ${CONTAINER_NAME} /usr/local/bin/composer'
 alias php='podman exec -it ${CONTAINER_NAME} /usr/bin/php'
